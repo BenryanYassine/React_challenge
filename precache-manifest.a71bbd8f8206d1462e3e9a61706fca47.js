eed34f6f958cfbaedcd1a1477ba6ed8625bfedc3
BenryanYassine <yassine.benryan@gmail.com>
2019-01-31 23:10:57 +0100

    Updates

---
+++ precache-manifest.a71bbd8f8206d1462e3e9a61706fca47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BenryanYassine/React_challenge/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a52b0d04e78391f53ee",
+    "url": "/BenryanYassine/React_challenge/static/js/runtime~main.4a52b0d0.js"
+  },
+  {
+    "revision": "a24781f1f4505371566d",
+    "url": "/BenryanYassine/React_challenge/static/js/main.a24781f1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/BenryanYassine/React_challenge/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a24781f1f4505371566d",
+    "url": "/BenryanYassine/React_challenge/static/css/main.190713b7.chunk.css"
+  },
+  {
+    "revision": "8607472e71f9c076fa66c57d57a713e1",
+    "url": "/BenryanYassine/React_challenge/index.html"
+  }
+];
